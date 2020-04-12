4fa5ac5a36091140d4395caa3a24b1faa276e3f3
Bill Goggin <bgoggin@mac.com>
2020-04-12 17:50:52 -0400

    Client side tests and coverage work.

---
+++ tdd/client/palindrome/src/palindrome.js
@@ -0,0 +1,4 @@
+let isPalindrome = function(phrase) {
+    if(phrase === undefined) { throw new Error('Invalid argument');}
+    return phrase.trim().length > 0 && phrase.split('').reverse().join('') === phrase;
+};
